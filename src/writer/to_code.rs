3d8dc872b3a856cc8386c94760c1b13ab90a34d3
Zachary Catlin <z@zc.is>
2021-02-26 14:20:01 -0500

    Removed explicit drop() call
    See https://github.com/gtk-rs/gir/pull/1062#discussion_r583859826
    for context.

--- src/writer/to_code.rs
+++ src/writer/to_code.rs
@@ -140,7 +140,7 @@ impl ToCode for Chunk {
                         r#"let signal_name: &[u8] = detailed_signal_name.as_ref().map_or(&b"{0}\0"[..], |n| n.as_bytes());"#,
                         signal
                     ));
-                    v.push("let handler_id = connect_raw(self.as_ptr() as *mut _, signal_name.as_ptr() as *const _,".to_string());
+                    v.push("connect_raw(self.as_ptr() as *mut _, signal_name.as_ptr() as *const _,".to_string());
                 } else {
                     v.push(format!(
                         "connect_raw(self.as_ptr() as *mut _, b\"{}\\0\".as_ptr() as *const _,",
@@ -152,10 +152,6 @@ impl ToCode for Chunk {
                     "\tSome(transmute::<_, unsafe extern \"C\" fn()>({}::<{}F> as *const ())), Box_::into_raw(f))",
                     trampoline, self_str
                 ));
-                if is_detailed {
-                    v.push("; drop(detailed_signal_name);".to_string());
-                    v.push("handler_id".to_string());
-                }
                 v
             }
             Name(ref name) => vec![name.clone()],
